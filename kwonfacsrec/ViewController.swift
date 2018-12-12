1dcb2b8a3581e269b5c0ccd802652161c18bda3e
권영진 <yjkwon@locusui-imac-2.local>
2018-12-12 12:08:08 +0900

    Initial Commit

---
+++ kwonfacsrec/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  kwonfacsrec
+//
+//  Created by 권영진 on 12/12/2018.
+//  Copyright © 2018 권영진. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
